95f4ba61ea81d646b6cbe0e898b996443084bf4a
Fernando <fernandonogue2003@gmail.com>
2024-08-08 23:59:18 -0300

    Revert "condicion derrota"
    This reverts commit de0ee0675db86c50f39da03b71857800f648983c.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 public class GameManager : MonoBehaviour
 {
-    public Jugador player;
     public CollectibleSystem collectibleSystem;  
-    public int pointsToWin;
+    public int pointsToWin;  
 
     void Update()
     {
         CheckWinCondition();
-        CheckLoseCondition();
     }
 
     public void CheckWinCondition()
@@ -21,24 +19,11 @@ public class GameManager : MonoBehaviour
             WinCondition();
         }
     }
-    public void CheckLoseCondition()
-    {
-        if (player.life <= 0)
-        {
-            LoseCondition();
-        }
-    }
 
-
+   
     public void WinCondition()
     {
-        SceneManager.LoadSceneAsync("victoria");
         Debug.Log("You Win!");
-    }
-
-    public void LoseCondition()
-    {
-        SceneManager.LoadSceneAsync("derrota");
-        Debug.Log("You Lose!");
+       
     }
 }
